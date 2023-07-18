b23caf4b38937bfe57a81977dc7d0e27217b08fe
Opet321 <90963402+Opet321@users.noreply.github.com>
2023-07-18 10:21:09 +0700

    Update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ except ValueError:
 # Pesan Saat Memaksa Subscribe
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\nSilakan Join Ke Channel & Group Terlebih Dahulu</b>",
+    "<b>Hello {first}\n\nUntuk terkait informasi loker kerja anda dapat bergabung ke grup dan channel berikut ini.</b>",
 )
 
 # Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom

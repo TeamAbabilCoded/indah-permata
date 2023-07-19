61b1ff8416ceba51f88bf98d126b5f59ac8ac200
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2023-07-19 13:03:51 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5872318909:AAHSkM5s-EogqT0_w00yz4MCfTgi2TszZyM")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6287683461:AAFGkC4AXcG-2B5yXJFutNdztSKEZ3XhSEk")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "11631070"))
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "11631070"))
 API_HASH = os.environ.get("API_HASH", "4f728d7b5419c457cecebbc5b4fc4036")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001888005970"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001770532804"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "Lolllokit")
@@ -36,7 +36,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://bnlgpjaq:vfMKvwbWC6Thahz_6Kh5kJOGSZu3l8yA@horton.db.elephantsql.com/bnlgpjaq")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://fzdrgpqv:0y3XurcWznmXbj9dIqvzVfsTEtANeqMF@tyke.db.elephantsql.com/fzdrgpqv")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001846135217"))

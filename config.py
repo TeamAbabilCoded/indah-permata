b99f54b0860d5650e458d33e0ab89e2e382179a4
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2023-08-08 00:25:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6287683461:AAFGkC4AXcG-2B5yXJFutNdztSKEZ3XhSEk")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6552577457:AAHh_UsOIaJsXcDu8NBNL_WpMoWWlRzXwqw")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "11631070"))
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "11631070"))
 API_HASH = os.environ.get("API_HASH", "4f728d7b5419c457cecebbc5b4fc4036")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001770532804"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001962950352"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "Lolllokit")
@@ -39,8 +39,8 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://fzdrgpqv:0y3XurcWznmXbj9dIqvzVfsTEtANeqMF@tyke.db.elephantsql.com/fzdrgpqv")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001846135217"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001919371724"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001726890410"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001716206036"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -57,7 +57,7 @@ except ValueError:
 # Pesan Saat Memaksa Subscribe
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b>Hello {first}\n\nUntuk terkait informasi loker kerja anda dapat bergabung ke grup dan channel berikut ini.</b>",
+    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\n Silakan Join Ke Channel & Group Terlebih Dahulu.</b>",
 )
 
 # Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
